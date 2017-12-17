11e9b6105c2865c624560e8f9a39368c35c1bdd5
deddy11 <30648100+deddy11@users.noreply.github.com>
2017-12-17 14:34:32 +0700

    stuck with display score and game play simultaneous

--- FlappyBird_RTOS/FlappyBird_RTOS.ino
+++ FlappyBird_RTOS/FlappyBird_RTOS.ino
@@ -19,25 +19,41 @@ void ScanDMD(){
 
 #define segment_a 2
 #define segment_b 3
-#define segment_c 6
-#define segment_d 7
-#define segment_e 8
-#define segment_f 4
-#define segment_g 5
+#define segment_c 4
+#define segment_d 5
+#define segment_e A2
+#define segment_f A3
+#define segment_g A4
+
 #define decoder0 A0
 #define decoder1 A1
 
 int ButtonState;
 int x1=25, x2=27, x3=26;
 int y1=8, y2=7, y3=9;
+int score = 0;
 SemaphoreHandle_t xMutex;
 
 // define tasks
 void TaskWall( void *pvParameters );
 void TaskBird( void *pvParameters );
+void TaskButton( void *pvParameters );
+void TaskScore( void *pvParameters );
 
 void setup(void){
+  //Input
   pinMode(inpButton, INPUT);
+  //Output
+  pinMode(segment_a, OUTPUT);
+  pinMode(segment_b, OUTPUT);
+  pinMode(segment_c, OUTPUT);
+  pinMode(segment_d, OUTPUT);
+  pinMode(segment_e, OUTPUT);
+  pinMode(segment_f, OUTPUT);
+  pinMode(segment_g, OUTPUT);
+  
+  pinMode(decoder0, OUTPUT);
+  pinMode(decoder1, OUTPUT);
   
   //initialize TimerOne’s interrupt/CPU usage used to scan and refresh the display
   Timer1.initialize( 2000 ); //period in microseconds to call ScanDMD. Anything longer than 5000 (5ms) and you can see flicker.
@@ -50,27 +66,46 @@ void setup(void){
   xMutex = xSemaphoreCreateMutex();
 
   if(xMutex != NULL) {
-    //  xTaskCreate(TaskWall,(const portCHAR *)"Display Wall",128,NULL,1,NULL);
-    xTaskCreate(TaskBird,(const portCHAR *)"Display Bird",128,NULL,1,NULL);
+    xTaskCreate(TaskWall,(const portCHAR *)"Display Wall",128,NULL,2,NULL);
+    xTaskCreate(TaskBird,(const portCHAR *)"Display Bird",128,NULL,2,NULL);
     xTaskCreate(TaskButton,(const portCHAR *)"Read Button",128,NULL,3,NULL);
+    xTaskCreate(TaskScore,(const portCHAR *)"Display Score",128,NULL,1,NULL);
     
     vTaskStartScheduler();
   }
 }
 
 void loop(void){
+}
 
+void TaskScore(void *pvParameters) { // This is a task.
+  int digit = 0;
+  for (;;) {
+    digitalWrite(decoder0, LOW);
+    digitalWrite(decoder1, LOW);
+    bcd(score%10);
+    delay(1);
+    digitalWrite(decoder0, HIGH);
+    digitalWrite(decoder1, LOW);
+    bcd((score%100)/10);
+    delay(1);
+    digitalWrite(decoder0, LOW);
+    digitalWrite(decoder1, HIGH);
+    bcd(score/100);
+    delay(1);
+    
+    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for 1ms
+  }
 }
 
 void TaskButton(void *pvParameters) { // This is a task.
-    
   for (;;) {
     ButtonState = debounceButton(ButtonState, inpButton);
-
     if(ButtonState == HIGH) {
       xSemaphoreTake(xMutex, portMAX_DELAY);
       {
-        dmd.clearScreen( true );
+        dmd.drawLine( x1, y1, x2, y1, GRAPHICS_INVERSE);
+        dmd.drawLine( x3, y2, x3, y3, GRAPHICS_INVERSE);
         y1 = y1 + 1;
         y2 = y2 + 1;
         y3 = y3 + 1;
@@ -79,7 +114,6 @@ void TaskButton(void *pvParameters) { // This is a task.
       }
       xSemaphoreGive(xMutex);
     }
-    
     vTaskDelay( 50 / portTICK_PERIOD_MS ); // wait for 50ms
   }
 }
@@ -88,7 +122,8 @@ void TaskBird(void *pvParameters) { // This is a task.
   for (;;) {    
     xSemaphoreTake(xMutex, portMAX_DELAY);
     {
-      dmd.clearScreen( true );
+      dmd.drawLine( x1, y1, x2, y1, GRAPHICS_INVERSE);
+      dmd.drawLine( x3, y2, x3, y3, GRAPHICS_INVERSE);
       y1 = y1 - 1;
       y2 = y2 - 1;
       y3 = y3 - 1;
@@ -102,7 +137,6 @@ void TaskBird(void *pvParameters) { // This is a task.
 
 void TaskWall(void *pvParameters) { // This is a task.
   int xWall1, xWall2, xWall3, xWall4; // variable for scrolling the wall
-  int skor; // each time a wall vanished, score is incremented
   int tinggibwh1, tinggibwh2, tinggibwh3, tinggibwh4; // the height of the low part of the wall
   int lebar = 3; // lebar wall 3 pixel
   int tinggi = 16; // tinggi total wall
@@ -117,15 +151,10 @@ void TaskWall(void *pvParameters) { // This is a task.
   xWall2 = xWall1-8;
   xWall3 = xWall1-16;
   xWall4 = xWall1-24;
-
-  // inisiasi skor awal game
-  skor = 0;
   
   for (;;) {
     // hilangkan wall untuk nanti dimunculkan lagi pada loop berikutnya
     dmd.clearScreen( true );
-    
-    Serial.println(skor);
     // gambar wall pertama
     dmd.drawFilledBox(xWall1,0,xWall1+2,tinggibwh1,GRAPHICS_NORMAL);
     dmd.drawFilledBox(xWall1,(tinggibwh1 + 6),xWall1+2,15,GRAPHICS_NORMAL);
@@ -148,28 +177,62 @@ void TaskWall(void *pvParameters) { // This is a task.
      * awal x = 0 saat kembali ke posisi x = 0, tinggi dari setiap wall juga diubah dengan 
      * tinggi yang baru setiap kali ada wall yang kembali ke posisi x = 0, skor bertambah 1
      */
+//    if(xWall1 = 30) {
+//      score++;
+//    } 
+//    else if(xWall2 = 30) {
+//      score++;
+//    }
+//    else if(xWall3 = 30) {
+//      score++;
+//    }
+//    else if(xWall4 = 30) {
+//      score++;
+//    }
+     
     if(xWall1 >= 32) {
       xWall1 = 0;
       tinggibwh1 = random(1, 10);
-      skor++;
     }
     if(xWall2 >= 32) {
       xWall2 = 0;
       tinggibwh2 = random(1, 10);
-      skor++;
     }
     if(xWall3 >= 32) {
       xWall3 = 0;
       tinggibwh3 = random(1, 10);
-      skor++;
     }
     if(xWall4 >= 32) {
       xWall4 = 0;
       tinggibwh4 = random(1, 10);
-      skor++;
     }
     vTaskDelay( 250 / portTICK_PERIOD_MS ); // wait for 250ms
   }
+}
+
+void bcd(byte bil)  {
+  byte D3, D2, D1, D0, A, B, C, D, E, F, G;
+  
+  D3 = (bil & 0b00001000);
+  D2 = (bil & 0b00000100);
+  D1 = (bil & 0b00000010);
+  D0 = (bil & 0b00000001);
+
+  A = (D3 || !D2 || D1 || D0) && (D3 || D2 || D1 || !D0); 
+  B = (D3 || !D2 || D1 || !D0) && (D3 || !D2 || !D1 || D0); 
+  C = D3 || D2 || !D1 || D0; 
+  D = (D3 || !D2 || D1 || D0) && (D3 || D2 || D1 || !D0) && (D3 || !D2 || !D1 || !D0); 
+  E = (!D2 && !D1 && !D0) || (D1 && !D0); 
+  F = (D3 || D2 || !D0) && (D3 || D2 || !D1) && (D3 || !D1 || !D0); 
+  G = (D3 || D2 || D1) && (D3 || !D2 || !D1 || !D0); 
+
+  digitalWrite(segment_a, !A);
+  digitalWrite(segment_b, !B);
+  digitalWrite(segment_c, !C);
+  digitalWrite(segment_d, !D);
+  digitalWrite(segment_e, !E);
+  digitalWrite(segment_f, !F);
+  digitalWrite(segment_g, !G);
 }
 
 int debounceButton(int stateButton, int pin) {
